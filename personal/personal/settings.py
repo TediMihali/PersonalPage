71c4e374942b1d4ef8ce24116ec85680f886c5dd
Tedi Mihali <tedimihali1@gmail.com>
2024-08-20 11:05:43 +0200

    Update settings.py

--- personal/personal/settings.py
+++ personal/personal/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "tedimihali1@gmail.com"
-EMAIL_HOST_PASSWORD = "iejw stxv pxmk tctb"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
